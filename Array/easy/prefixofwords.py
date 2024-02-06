20241148997d7032a0913f0a68d55ace33e287b9
Punit Jain <contactpunitjain@gmail.com>
2024-02-06 13:30:40 +0530

    perfect solution

--- Array/easy/prefixofwords.py
+++ Array/easy/prefixofwords.py
@@ -17,50 +17,36 @@
 # Output: false
 # Explanation:
 # It is impossible to make s using a prefix of arr.
- 
-from itertools import accumulate
 
 def isPrefixString(s, words):
-    return s in accumulate(words)
-    # start = 0
-    # strlen = len(s)
-    # end = strlen - 1
-    # for word in words:
-    #     if start > end:
-    #         return True
-    #     for char in word:
-    #         if start > end:
-    #             return False
-    #         elif s[start] != char:
-    #             return False
-    #         else:
-    #             start += 1
-    # if start > end:
-    #     return True
-    # else:
-    #     return False
+    n = len(words)
+    for i in range(n):
+        print("".join(words[:i+1]), s)
+        if "".join(words[:i+1]) == s:
+            return True
+    return False
         
 
 s = "iloveleetcode"
 words = ["i","love","leetcode","apples"]
 print(isPrefixString(s, words))
 
-s = "iloveleetcode"
-words = ["apples","i","love","leetcode"]
-print(isPrefixString(s, words))
+# s = "iloveleetcode"
+# words = ["apples","i","love","leetcode"]
+# print(isPrefixString(s, words))
 
-s = "a"
-words = ["aa","aaaa","banana"]
-print(isPrefixString(s, words))
+# s = "a"
+# words = ["aa","aaaa","banana"]
+# print(isPrefixString(s, words))
 
-s="aaa"
-words = ["aa","aaa","fjaklfj"]
-print(isPrefixString(s, words))
+# s="aaa"
+# words = ["aa","aaa","fjaklfj"]
+# print(isPrefixString(s, words))
 
-s="z"
-words = ["z"]
-print(isPrefixString(s, words))
+# s="z"
+# words = ["z"]
+# print(isPrefixString(s, words))
 
-s ="aaaaaaa"
-words =["a","a","a","a","a","a"]
-print(isPrefixString(s, words))
+# s ="aaaaaaa"
+# words =["a","a","a","a","a","a"]
+# print(isPrefixString(s, words))
