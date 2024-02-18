562dc88203003ac290c54b713b204d5b8992760a
Punit Jain <contactpunitjain@gmail.com>
2024-02-18 21:57:23 +0530

    easiest solution.. consize

--- Array/easy/longpressedname.py
+++ Array/easy/longpressedname.py
@@ -24,46 +24,32 @@
 # name and typed consist of only lowercase English letters.
 
 def isLongPressedName(name, typed):
-    j = 0
-    jlen = len(typed)
-    ilen = len(name)
-    if name == typed:
-        return True
-    if ilen > jlen:
-        return False
-    for i in range(ilen):
-        if j >= jlen: return False
-        if name[i] != typed[j]:
-            if j == 0: return False
-            while j < jlen and typed[j] == typed[j - 1]:
-                j += 1
-            if j < jlen and typed[j] != name[i]:
-                return False
-            if j == jlen and i < ilen:
-                return False   
-        j += 1
-    if j < jlen:
-        while j < jlen:
-            if typed[j] != typed[j -1]:
-                return False
+    i, j = 0, 0
+    while j < len(typed):
+        if i < len(name) and name[i] == typed[j]:
+            i += 1
             j += 1
-    return True 
+        elif j > 0 and typed[j-1] == typed[j]:
+            j += 1
+        else:
+            return False
+    return i == len(name) 
 
-# name = "alex"
-# typed = "aaleex"
-# print(isLongPressedName(name, typed))
+name = "alex"
+typed = "aaleex"
+print(isLongPressedName(name, typed))
 
-# name = "saeed"
-# typed = "ssaaedd"
-# print(isLongPressedName(name, typed))
+name = "saeed"
+typed = "ssaaedd"
+print(isLongPressedName(name, typed))
 
-# name = 'a'
-# typed ='b'
-# print(isLongPressedName(name, typed))
+name = 'a'
+typed ='b'
+print(isLongPressedName(name, typed))
 
-# name = "alex"
-# typed = "aaleexa"
-# print(isLongPressedName(name, typed))
+name = "alex"
+typed = "aaleexa"
+print(isLongPressedName(name, typed))
 
 name = "pyplrz"
 typed = "ppyypllr"
