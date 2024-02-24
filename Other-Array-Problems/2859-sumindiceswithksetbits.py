bfd2058c31504459a1c1463b5fab67c6b8787fa5
Punit Jain <contactpunitjain@gmail.com>
2024-02-24 14:48:52 +0530

    sum indeces with kset bits

---
+++ Other-Array-Problems/2859-sumindiceswithksetbits.py
@@ -0,0 +1,50 @@
+# You are given a 0-indexed integer array nums and an integer k.
+
+# Return an integer that denotes the sum of elements in nums whose corresponding indices
+# have exactly k set bits in their binary representation.
+
+# The set bits in an integer are the 1's present when it is written in binary.
+
+# For example, the binary representation of 21 is 10101, which has 3 set bits.
+ 
+# Example 1:
+
+# Input: nums = [5,10,1,5,2], k = 1
+# Output: 13
+# Explanation: The binary representation of the indices are: 
+# 0 = 000
+# 1 = 001
+# 2 = 010
+# 3 = 011
+# 4 = 100
+# Indices 1, 2, and 4 have k = 1 set bits in their binary representation.
+# Hence, the answer is nums[1] + nums[2] + nums[4] = 13.
+# Example 2:
+
+# Input: nums = [4,3,2,1], k = 2
+# Output: 1
+# Explanation: The binary representation of the indices are:
+# 0 = 002
+# 1 = 012
+# 2 = 102
+# 3 = 112
+# Only index 3 has k = 2 set bits in its binary representation.
+# Hence, the answer is nums[3] = 1.
+
+def sumIndicesWithKSetBits(nums, k):
+    result = 0
+    i = 0
+    for num in nums:
+        currBinary = bin(i)[2:]
+        i += 1
+        if currBinary.count('1') == k:
+            result += num
+    return result
+
+nums = [4,3,2,1]
+k = 2
+print(sumIndicesWithKSetBits(nums, k))
+
+nums = [5,10,1,5,2]
+k = 1
+print(sumIndicesWithKSetBits(nums, k))
