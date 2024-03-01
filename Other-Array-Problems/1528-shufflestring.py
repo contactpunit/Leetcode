174f9410be3beaf28f32f4281d6800d7017218c5
Punit Jain <contactpunitjain@gmail.com>
2024-03-01 16:45:58 +0530

    shuffle using zip

---
+++ Other-Array-Problems/1528-shufflestring.py
@@ -0,0 +1,27 @@
+# You are given a string s and an integer array indices of the same length.
+# The string s will be shuffled such that the character at the ith position moves to indices[i]
+# in the shuffled string.
+
+# Return the shuffled string.
+
+# Example 1:
+
+# Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+# Output: "leetcode"
+# Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+# Example 2:
+
+# Input: s = "abc", indices = [0,1,2]
+# Output: "abc"
+# Explanation: After shuffling, each character remains in its position.
+
+def restoreString(s, indices):
+    final = [0]*len(indices)
+    zipped = list(zip(s, indices))
+    for pair in zipped:
+        final[pair[1]] = pair[0]
+    return ''.join(final)
+
+s = "codeleet"
+indices = [4,5,6,7,0,2,1,3]
+print(restoreString(s, indices))
