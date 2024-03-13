bb323edea51afad2b5b0d47fad4e2b7d0920c30a
Punit Jain <contactpunitjain@gmail.com>
2024-03-13 21:36:09 +0530

    element max frequency

---
+++ Other-Array-Problems/3005-elemmaxfrequency.py
@@ -0,0 +1,34 @@
+# You are given an array nums consisting of positive integers.
+
+# Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+# The frequency of an element is the number of occurrences of that element in the array.
+
+# Example 1:
+
+# Input: nums = [1,2,2,3,1,4]
+# Output: 4
+# Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+# So the number of elements in the array with maximum frequency is 4.
+# Example 2:
+
+# Input: nums = [1,2,3,4,5]
+# Output: 5
+# Explanation: All elements of the array have a frequency of 1 which is the maximum.
+# So the number of elements in the array with maximum frequency is 5.
+
+from collections import Counter
+def maxFrequencyElements(nums):
+    freq = 0
+    maxvalue = 0
+    c = Counter(nums)
+    for k, v in c.items():
+        if v == maxvalue:
+            freq += c[k]
+        elif v > maxvalue:
+            maxvalue = v
+            freq = c[k]
+    return freq
+
+nums = [1,2,2,3,1,4]
+print(maxFrequencyElements(nums))
