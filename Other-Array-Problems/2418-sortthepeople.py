c63633ab2703b65e764fe052f9c2de8e8a274ad7
Punit Jain <contactpunitjain@gmail.com>
2024-03-14 10:19:40 +0530

    sort array baed on another array

---
+++ Other-Array-Problems/2418-sortthepeople.py
@@ -0,0 +1,26 @@
+# You are given an array of strings names, and an array heights that consists of distinct 
+# positive integers. Both arrays are of length n.
+
+# For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+# Return names sorted in descending order by the people's heights.
+
+# Example 1:
+
+# Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+# Output: ["Mary","Emma","John"]
+# Explanation: Mary is the tallest, followed by Emma and John.
+# Example 2:
+
+# Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+# Output: ["Bob","Alice","Bob"]
+# Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+def sortPeople(names, heights):
+    matched = zip(names, heights)
+    sortMatched = sorted(matched, key=lambda a: a[1], reverse=True)
+    return [pair[0] for pair in sortMatched]
+
+names = ["Mary","John","Emma"]
+heights = [180,165,170]
+print(sortPeople(names, heights))
