61f0cb273d713d1b243133066bf07289a0662040
Punit Jain <contactpunitjain@gmail.com>
2024-03-16 22:12:44 +0530

    append and print elements of list

--- LinkedList/CreatingLinkedList.py
+++ LinkedList/CreatingLinkedList.py
@@ -10,9 +10,24 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        temp = Node(value)
+        if self.head == None:
+            self.head = temp
+            self.tail = temp
+        else:
+            self.tail.next = temp
+            self.tail = temp
+        self.length += 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
 l_list1 = LinkedList(4)
-l_list2 = LinkedList(6)
-print(l_list1)
-print(l_list1.head.value)
-print(l_list1.tail.value)
-print(l_list2)
+l_list1.append(5)
+l_list1.append(7)
+l_list1.append(1)
+l_list1.print_list()
