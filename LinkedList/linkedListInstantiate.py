9e07da6bae5f0c197b42a5c42fea455c0702d155
Punit Jain <contactpunitjain@gmail.com>
2024-03-31 19:29:55 +0530

    instantiate a linked list

---
+++ LinkedList/linkedListInstantiate.py
@@ -0,0 +1,22 @@
+# forst create a Node class which instantiate each new node
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+node1 = Node('elem1')
+head = node1
+
+node2 = Node('elem2')
+node3 = Node('elem3')
+node4 = Node('elem4')
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+pointer = head
+
+while pointer != None:
+    print(pointer.value)
+    pointer = pointer.next
